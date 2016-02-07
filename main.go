a82b1cfb7b646b128b854b18c56ab289408af572
Pasang Sherpa <pasang_sh@hotmail.com>
2016-02-06 23:19:47 -0800

    main file name changed

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+  "encoding/json"
+  "fmt"
+  "github.com/pasangsherpa/go-screen-capture/Godeps/_workspace/src/github.com/julienschmidt/httprouter"
+  "log"
+  "net/http"
+  "os"
+)
+
+func Index(res http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+  fmt.Fprintf(res, "welcome to screen capture service")
+}
+
+func Pong(res http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+  data := map[string]interface{}{
+    "response": "pong",
+  }
+  js, _ := json.Marshal(data)
+  res.Header().Set("Content-Type", "application/json")
+  res.Write(js)
+}
+
+func main() {
+  router := httprouter.New()
+  router.GET("/", Index)
+  router.GET("/ping", Pong)
+
+  fmt.Println("listening @port:" + os.Getenv("PORT"))
+  log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
+}
