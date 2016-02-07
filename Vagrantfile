0562645408fe027a967d3e01d267cfd3d1639d7b
Pasang Sherpa <pasang_sh@hotmail.com>
2016-02-06 20:12:08 -0800

    - api updated with httprouter - health check added - vagrant file modified - doc updated

--- Vagrantfile
+++ Vagrantfile
@@ -15,4 +15,6 @@ Vagrant.configure(2) do |config|
   config.vm.network "forwarded_port", guest: 9090, host: 9090
   config.vm.network "forwarded_port", guest: 9091, host: 9091
 
+  config.vm.synced_folder ".", "/home/vagrant/go/src/github.com/pasangsherpa/go-screen-capture"
+
 end
--- web.go
+++ web.go
@@ -1,20 +1,32 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "os"
+	"encoding/json"
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"log"
+	"net/http"
+	"os"
 )
 
-func main() {
-  http.HandleFunc("/", handler)
-  fmt.Println("listening @port:" + os.Getenv("PORT"))
-  err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
-  if err != nil {
-    panic(err)
-  }
+func Index(res http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+
 }
 
-func handler(res http.ResponseWriter, req *http.Request) {
-  fmt.Fprintln(res, "screen capture service!!")
+func Pong(res http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	data := map[string]interface{}{
+		"response": "pong",
+	}
+	js, _ := json.Marshal(data)
+	res.Header().Set("Content-Type", "application/json")
+	res.Write(js)
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/ping", Pong)
+
+	fmt.Println("listening @port:" + os.Getenv("PORT"))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router))
 }
