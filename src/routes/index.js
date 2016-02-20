fee952dec62fa22cdc97a9a9efb887c177f89b61
Pasang Sherpa <pasang_sh@hotmail.com>
2016-02-19 23:15:12 -0800

    phantoms integrated

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@
  */
 
 const co = require('co');
+const phantom = require('phantom');
 const Router = require('koa-router');
 
 /**
@@ -18,8 +19,46 @@ const router = new Router();
  */
 
 router.get('/', co.wrap(function* (ctx, next) {
+  let ph, page, url = ctx.query.url;
+  if (!url){
+    return ctx.body = {
+      error: {
+        message: 'url query param is required'
+      }
+    }
+  }
+  return phantom.create()
+    .then(p => {
+      ph = p;
+      return ph.createPage();
+    })
+    .then(p => {
+      page = p;
+      return page.open(url);
+    })
+    .then(status => {
+      if (status === 'success') {
+        return page.property('content');
+      }
+      return null;
+    })
+    .then(content => {
+      let message = content ? content : 'Failed';
+      page.close();
+      ph.exit();
+      ctx.body = {
+        message: message
+      }
+    });
+}));
+
+/**
+ * Healthcheck
+ */
+
+router.get('/ping', co.wrap(function* (ctx, next) {
   ctx.body = {
-    message: 'nested pong'
+    message: 'pong'
   };
 }));
 
