896a7d645901eca6488fcab2aaaa7b4db6ad5ec2
Pasang Sherpa <pasang_sh@hotmail.com>
2016-02-23 22:49:26 -0800

    es6 fix

--- test/utils.js
+++ test/utils.js
@@ -1,9 +1,10 @@
 'use strict';
-var assert = require('assert');
-var utils = require('src/lib/utils');
 
-describe('utils', function () {
-  it('should generate md5 of given string', function () {
+const assert = require('assert');
+const utils = require('src/lib/utils');
+
+describe('utils', () => {
+  it('should generate md5 of given string', () => {
     const foo_md5ed = 'acbd18db4cc2f85cedef654fccc4a4d8';
     const github_md5ed = 'bf215181b5140522137b3d4f6b73544a';
     assert.equal(utils.md5('foo'), foo_md5ed);
